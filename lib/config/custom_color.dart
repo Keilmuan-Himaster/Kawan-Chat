0c7d8cd7bc970bb9a1810d48a71f7e84d578559b
Hutomo Sakti Kartiko <hutomosaktikartiko.siskom@gmail.com>
2021-09-02 23:48:24 +0700

    Add custom color & text style

---
+++ lib/config/custom_color.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class BrandColor {
+  final Color dark = Color(0xFF001A83);
+  final Color defaultColor = Color(0xFF002DE3);
+  final Color darkMode = Color(0xFF375FFF);
+  final Color light = Color(0xFF879FFF);
+  final Color background = Color(0xFFD2D5F9);
+}
+
+class NeutralColor {
+  final Color active = Color(0xFF0F1828);
+  final Color dark = Color(0xFF152033);
+  final Color body = Color(0xFF1B2B48);
+  final Color weak = Color(0xFFA4A4A4);
+  final Color disabled = Color(0xFFADB5BD);
+  final Color line = Color(0xFFEDEDED);
+  final Color offWhite = Color(0xFFF7F7FC);
+  final Color white = Color(0xFFFFFFFF);
+}
+
+class AccentColor {
+  final Color danger = Color(0xFFE94242);
+  final Color warning = Color(0xFFFDCF41);
+  final Color success = Color(0xFF2CC069);
+  final Color safe = Color(0xFF7BCBCF);
+}
