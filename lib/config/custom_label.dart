cf44188b2ecc3473750acaacb309c6d36a61883d
Hutomo Sakti Kartiko <hutomosaktikartiko.siskom@gmail.com>
2021-09-02 23:56:07 +0700

    Add custom label

---
+++ lib/config/custom_label.dart
@@ -0,0 +1,7 @@
+class CustomLabel {
+  final String splashLabel = "Connect easily with\nyour family and friends\nover countries";
+  final String loginLabel = "Enter Your Phone Number";
+  final String loginDescription = "Please confirm your country code and enter\nyour phone number";
+  final String verificationLabel = "Enter Code";
+  final String verifitcaionDescription = "We have sent you an SMS with the code\nto";
+}
