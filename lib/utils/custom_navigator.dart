14d721d973fc30699cd35bcc841975953ccd2513
Hutomo Sakti Kartiko <hutomosaktikartiko.siskom@gmail.com>
2021-09-02 23:51:50 +0700

    Custom navigator

---
+++ lib/utils/custom_navigator.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CustomNavigator {
+  startScreen(BuildContext context, Widget screen) =>
+      Navigator.push(context, MaterialPageRoute(builder: (context) => screen));
+
+  replaceScreen(BuildContext context, Widget screen) =>
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => screen));
+
+  removeScreen(BuildContext context, Widget screen) =>
+      Navigator.pushAndRemoveUntil(context,
+          MaterialPageRoute(builder: (context) => screen), (route) => false);
+
+  closeScreen<T>(BuildContext context, [T? value]) => Navigator.pop<T>(context);
+}
