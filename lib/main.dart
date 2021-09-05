33c98f44fcb8ecde951a98c8997827ed3f89540a
Hutomo Sakti Kartiko <hutomosaktikartiko.siskom@gmail.com>
2021-09-05 18:39:05 +0700

    Firebase Configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:chat_app/config/custom_color.dart';
 import 'package:chat_app/ui/screens/onboarding_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(MyApp());
 }
 
